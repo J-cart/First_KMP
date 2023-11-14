e57232ba86d5310b115c65568b6ecb48c6277ed5
Joseph <j.t.cartoonist@gmail.com>
2023-11-14 19:59:13 +0100

    created "create note group ","note group note items" screens ....

---
+++ shared/src/commonMain/kotlin/com/tutorials/firstkmp/presentation/screen/NewNoteGroupScreen.kt
@@ -0,0 +1,129 @@
+package com.tutorials.firstkmp.presentation.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NewNoteGroupScreen() {
+    var noteGroupTitle by remember {
+        mutableStateOf("")
+    }
+    var someText by remember { mutableStateOf(TextFieldValue("")) }
+
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(text = "New note group", modifier = Modifier.padding(start = 10.dp)) },
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    contentDescription = "Back"
+                )
+            },
+            actions = {
+                IconButton(onClick = { /*TODO: delete note group*/ }) {
+                    Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+                }
+            }
+        )
+    }, floatingActionButton = {
+        FloatingActionButton(
+            shape = CircleShape,
+            onClick = { /*TODO: navigate to note group items*/ }) {
+            Icon(imageVector = Icons.Default.Check, contentDescription = "Continue")
+        }
+    }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Spacer(modifier = Modifier.width(12.dp))
+                Image(
+                    modifier = Modifier
+                        .size(56.dp)
+                        .background(color = Color.LightGray, shape = CircleShape)
+                        .clickable {
+                            // TODO: select group image
+                        }
+                        .padding(4.dp)
+                        .align(Alignment.Bottom),
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Clear"
+                )
+
+                Spacer(modifier = Modifier.width(6.dp))
+                TextField(
+                    value = noteGroupTitle,
+                    onValueChange = {
+                        noteGroupTitle = it
+                    },
+                    placeholder = {
+                        Text(text = "Note group title", color = Color.LightGray)
+                    },
+                    singleLine = true,
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                    )
+                )
+                Spacer(modifier = Modifier.width(6.dp))
+                Image(
+                    modifier = Modifier
+                        .size(24.dp)
+                        .border(width = 1.dp, shape = CircleShape, color = Color.LightGray)
+                        .clickable { noteGroupTitle = "" }
+                        .padding(4.dp),
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "Clear"
+                )
+                Spacer(modifier = Modifier.width(12.dp))
+            }
+        }
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/tutorials/firstkmp/presentation/screen/NoteGroupItemScreen.kt
@@ -0,0 +1,186 @@
+package com.tutorials.firstkmp.presentation.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteGroupItemScreen(
+    groupTitle: String
+) {
+    var noteText by remember {
+        mutableStateOf("")
+    }
+
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text(text = groupTitle, modifier = Modifier.padding(start = 10.dp)) },
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    contentDescription = "Back"
+                )
+            },
+            actions = {
+                IconButton(onClick = { /*TODO: edit note group*/ }) {
+                    Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+                }
+            }
+        )
+    }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            Box(modifier = Modifier.fillMaxWidth().height(0.8.dp).background(color = Color.LightGray).padding(top = 10.dp))
+
+            Box(modifier = Modifier.weight(1f)) {
+                LazyColumn(
+                    horizontalAlignment = Alignment.End
+                ) {
+                    item {
+                        NoteItem(text = "Some text")
+                    }
+                    item {
+                        NoteItem(text = "SSha as far say e dey work and I no dey reason time-space complexity then whatever works is fine by me")
+                    }
+                    item {
+                        NoteItem(text = "SSha as far say e dey work and I no dey reason time-space ct")
+                    }
+                    item {
+                        NoteItem(text = "Sha as far say e dey work and I no dey reason time-space complexity then whatever works is fine by meSha as far say e dey work and I no dey reason time-space complexity then whatever works is fine by me")
+                    }
+                    item {
+                        NoteItem(text = "Sha as far say e dey work and I ")
+                    }
+                    item {
+                        NoteItem(text = "Some text")
+                    }
+                }
+
+            }
+
+
+            Column {
+                Box(modifier = Modifier.fillMaxWidth().height(0.8.dp).background(color = Color.LightGray))
+                Box {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 12.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        TextField(
+                            modifier = Modifier
+                                .fillMaxWidth(0.8f)
+                                .padding(end = 8.dp),
+                            value = noteText,
+                            placeholder = {
+                                Text(text = "What's on your mind?", color = Color.LightGray)
+                            },
+                            onValueChange = {
+                                noteText = it
+                            },
+                            colors = TextFieldDefaults.colors(
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                focusedContainerColor = Color.Transparent,
+                                unfocusedContainerColor = Color.Transparent,
+                            )
+                        )
+                        Icon(
+                            modifier = Modifier
+                                .clickable {
+                                    // TODO: select attachment
+                                }
+                                .padding(end = 8.dp),
+                            imageVector = Icons.Default.MoreVert, contentDescription = "Attachment"
+                        )
+                        FilledIconButton(
+                            modifier = Modifier
+                                .padding(end = 8.dp)
+                                .align(Alignment.Bottom),
+                            shape = CircleShape, onClick = {
+                                /*TODO: save note*/
+                            }) {
+                            Icon(imageVector = Icons.Default.Send, contentDescription = "Add note")
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NoteItem(text:String) {
+    Surface(
+        modifier = Modifier.padding(
+            top = 5.dp,
+            bottom = 5.dp,
+            end = 10.dp,
+            start = 10.dp
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .wrapContentSize()
+                .background(
+                    color = Color.Gray, shape = RoundedCornerShape(
+                        CornerSize(8.dp)
+                    )
+                )
+                .padding(8.dp)
+        ) {
+            Column {
+                Text(text = text)
+                Text(
+                    modifier = Modifier.align(Alignment.End),
+                    text = "7:34pm",
+                    color = Color.LightGray,
+                    fontSize = 12.sp
+                )
+            }
+        }
+    }
+}
